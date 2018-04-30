d0f13f82cb10694d36cd029a6590b0b93bb4001d
pseudoyim <paul.j.yim@gmail.com>
2018-04-30 09:28:39 -0400

    Search page currently only shows the last result.

--- book_rapport/shelves/static/js/search.js
+++ book_rapport/shelves/static/js/search.js
@@ -14,14 +14,28 @@ $('#search-button').on('click', function(){
 });
 
 
+
+// function handleResponse( response ) {
+//   var results_count = response.items.length;
+//   for (var i = results_count - 1; i >= 0; i--) {
+    
+//   }
+
+
+// }
+
+
+
 function handleResponse( response ) {
-  console.log(response.items);
+  // console.log(response.items);
   $.each( response.items, function( i, item ) {
     
     var title    = item.volumeInfo.title,
         author   = item.volumeInfo.authors[0],        
         thumb    = item.volumeInfo.imageLinks.thumbnail;
     
+
+
     $('.title').text( title );
     $('.author').text( author );
     $('.thumbnail').attr('src', thumb);
