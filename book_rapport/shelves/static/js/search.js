b10b74e4175eda4e39cbe39c76c85df4e4753053
pseudoyim <paul.j.yim@gmail.com>
2018-04-30 15:52:56 -0400

    Search page no populates all results from API call.

--- book_rapport/shelves/static/js/search.js
+++ book_rapport/shelves/static/js/search.js
@@ -15,29 +15,42 @@ $('#search-button').on('click', function(){
 
 
 
-// function handleResponse( response ) {
-//   var results_count = response.items.length;
-//   for (var i = results_count - 1; i >= 0; i--) {
-    
-//   }
-
-
-// }
-
-
-
 function handleResponse( response ) {
-  // console.log(response.items);
-  $.each( response.items, function( i, item ) {
-    
-    var title    = item.volumeInfo.title,
-        author   = item.volumeInfo.authors[0],        
-        thumb    = item.volumeInfo.imageLinks.thumbnail;
-    
+  $('.books').children().remove();
 
+  var results_count = response.items.length;
+  
+  for (var i = 0; i < results_count; i++) {
+    var div    = $('<div id=result' + i + '></div>'),
+        title  = response.items[i].volumeInfo.title,
+        author = response.items[i].volumeInfo.authors[0],        
+        thumb  = response.items[i].volumeInfo.imageLinks.thumbnail;
 
-    $('.title').text( title );
-    $('.author').text( author );
-    $('.thumbnail').attr('src', thumb);
-  });
+    var htm_thumb   = $('<img src="' + thumb + '" alt="thumb' + i + '" class="thumbnail" />'),
+        htm_title   = $('<h3>' + title + '</h3>'),
+        htm_author  = $('<h3>' + author + '</h3>');
+        
+    htm_thumb.appendTo(div);
+    htm_title.appendTo(div);
+    htm_author.appendTo(div);
+
+    div.appendTo('.books')
+  }
 }
+
+
+// function handleResponse( response ) {
+//   // console.log(response.items);
+//   $.each( response.items, function( i, item ) {
+    
+//     var title    = item.volumeInfo.title,
+//         author   = item.volumeInfo.authors[0],        
+//         thumb    = item.volumeInfo.imageLinks.thumbnail;
+    
+
+
+//     $('.title').text( title );
+//     $('.author').text( author );
+//     $('.thumbnail').attr('src', thumb);
+//   });
+// }
