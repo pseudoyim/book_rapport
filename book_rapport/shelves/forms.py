6828af9d13d758b2d474226068ba343c59b6857f
pseudoyim <paul.j.yim@gmail.com>
2018-05-21 20:13:11 -0400

    Cleaned up add_read page

--- book_rapport/shelves/forms.py
+++ book_rapport/shelves/forms.py
@@ -5,13 +5,17 @@ from .models import Book, Note
 class BookForm(forms.ModelForm):
 	class Meta:
 		model = Book
-		fields = ['title', 'author_name', 'publisher', 'ISBN', 'pub_date', 'read_start_date', 'read_end_date', 'user_rating', 'user_review']
+		fields = ['title', 'author_name', 'thumb_url', 'publisher', 'pub_date', 'pages', 'genre', 'isbn13', 'isbn10', 'read_start_date', 'read_end_date', 'user_rating', 'user_review']
 
 	title = forms.CharField(max_length=250)
 	author_name = forms.CharField(max_length=100)
+	thumb_url = forms.CharField(max_length=1000)
 	publisher = forms.CharField(max_length=250)
-	ISBN = forms.CharField(max_length=100)
 	pub_date = forms.DateField()
+	pages = forms.IntegerField()
+	genre = forms.CharField(max_length=250)
+	isbn13 = forms.CharField(max_length=100)
+	isbn10 = forms.CharField(max_length=100)
 	read_start_date = forms.DateField()
 	read_end_date = forms.DateField()
 	user_rating = forms.IntegerField()
--- book_rapport/shelves/static/js/add_read.js
+++ book_rapport/shelves/static/js/add_read.js
@@ -1,4 +1,14 @@
-var search_query
+var search_query,
+title,
+author,
+thumb,
+pub,
+pub_dt,
+pages,
+genre,
+isbn13,
+isbn10;
+
 
 $('#search-button').on('click', function(){
   console.log('search button clicked!');
@@ -12,23 +22,25 @@ $('#search-button').on('click', function(){
 });
 
 
-function handleResponse( response ) {
+function handleResponse(response) {
   $('.books').children().remove();
 
   var results_count = response.items.length;
   
   for (var i = 0; i < results_count; i++) {
+
+    title     = response.items[i].volumeInfo.title,
+    author    = response.items[i].volumeInfo.authors[0],
+    thumb     = response.items[i].volumeInfo.imageLinks.thumbnail,
+    pub       = response.items[i].volumeInfo.publisher,
+    pub_dt    = response.items[i].volumeInfo.publishedDate,
+    pages     = response.items[i].volumeInfo.pageCount,
+    genre     = response.items[i].volumeInfo.categories,
+    isbn13    = response.items[i].volumeInfo.industryIdentifiers[1].identifier,
+    isbn10    = response.items[i].volumeInfo.industryIdentifiers[0].identifier;
+
     var div_img   = $('<div id=result_img' + i + '></div>'),
         div_data  = $('<div id=result_data' + i + '></div>'),
-        title     = response.items[i].volumeInfo.title,
-        author    = response.items[i].volumeInfo.authors[0],
-        thumb     = response.items[i].volumeInfo.imageLinks.thumbnail,
-        pub       = response.items[i].volumeInfo.publisher,
-        pub_dt    = response.items[i].volumeInfo.publishedDate,
-        pages     = response.items[i].volumeInfo.pageCount,
-        genre     = response.items[i].volumeInfo.categories,
-        isbn13    = response.items[i].volumeInfo.industryIdentifiers[0].identifier,
-        isbn10    = response.items[i].volumeInfo.industryIdentifiers[1].identifier,
         htm_thumb   = $('<img src="' + thumb + '" alt="thumb' + i + '" class="thumbnail" />'),
         htm_title   = $('<h3>' + title + '</h3>'),
         htm_author  = $('<h4>' + author + '</h4>'),
@@ -36,21 +48,23 @@ function handleResponse( response ) {
         htm_pub_dt  = $('<p>Published: ' + pub_dt + '</p>'),
         htm_pages   = $('<p>Pages: ' + pages + '</p>'),
         htm_genre   = $('<p>Genre: ' + genre + '</p>'),
-        htm_isbn    = $('<p>ISBN_13: ' + isbn13 + '; ISBN_10: ' + isbn10 + '</p>'),
+        htm_isbn    = $('<p>ISBN_13: ' + isbn13 + '; ISBN_10: ' + isbn10 + '</p>');
 
     // Data to send to the add_read page.
-        input_title     = $('<input type="hidden" name="title" value="' + title + '"/>'),
-        input_author    = $('<input type="hidden" name="author" value="' + author + '"/>'),
-        input_thumb     = $('<input type="hidden" name="thumb" value="' + thumb + '"/>'),
-        input_pub       = $('<input type="hidden" name="pub" value="' + pub + '"/>'),
-        input_pub_dt    = $('<input type="hidden" name="pub_dt" value="' + pub_dt + '"/>'),
-        input_pages     = $('<input type="hidden" name="pages" value="' + pages + '"/>'),
-        input_genre     = $('<input type="hidden" name="genre" value="' + genre + '"/>'),
-        input_isbn13    = $('<input type="hidden" name="isbn13" value="' + isbn13 + '"/>'),
-        input_isbn10    = $('<input type="hidden" name="isbn10" value="' + isbn10 + '"/>');
+        // input_title     = $('<input type="hidden" name="title" value="' + title + '"/>'),
+        // input_author    = $('<input type="hidden" name="author" value="' + author + '"/>'),
+        // input_thumb     = $('<input type="hidden" name="thumb" value="' + thumb + '"/>'),
+        // input_pub       = $('<input type="hidden" name="pub" value="' + pub + '"/>'),
+        // input_pub_dt    = $('<input type="hidden" name="pub_dt" value="' + pub_dt + '"/>'),
+        // input_pages     = $('<input type="hidden" name="pages" value="' + pages + '"/>'),
+        // input_genre     = $('<input type="hidden" name="genre" value="' + genre + '"/>'),
+        // input_isbn13    = $('<input type="hidden" name="isbn13" value="' + isbn13 + '"/>'),
+        // input_isbn10    = $('<input type="hidden" name="isbn10" value="' + isbn10 + '"/>');
 
-    var add_button = $('<button type="submit" name="add-book">ADD BOOK</button>');
-    var form = $('<form action="/add_read/" method="GET"></form>')
+    // var add_button = $('<button type="submit" name="add-book">ADD BOOK</button>');
+    var add_button = $('<input type="button" id="add-book" value="Add Book"/>');
+    
+    // var form = $('<form action="/add_read/" method="GET"></form>')
 
     htm_thumb.appendTo(div_img);
     htm_title.appendTo(div_data);
@@ -61,23 +75,26 @@ function handleResponse( response ) {
     htm_genre.appendTo(div_data);
     htm_isbn.appendTo(div_data);
 
-    div_img.appendTo(form);
-    div_data.appendTo(form);
-    
+    // input_title.appendTo(form);
+    // input_author.appendTo(form);
+    // input_thumb.appendTo(form);
+    // input_pub.appendTo(form);
+    // input_pub_dt.appendTo(form);
+    // input_pages.appendTo(form);
+    // input_genre.appendTo(form);
+    // input_isbn13.appendTo(form);
+    // input_isbn10.appendTo(form);
 
-    input_title.appendTo(form);
-    input_author.appendTo(form);
-    input_thumb.appendTo(form);
-    input_pub.appendTo(form);
-    input_pub_dt.appendTo(form);
-    input_pages.appendTo(form);
-    input_genre.appendTo(form);
-    input_isbn13.appendTo(form);
-    input_isbn10.appendTo(form);
+    // add_button.appendTo(form);
 
-    add_button.appendTo(form);
-
-    form.appendTo('.books');
-
+    div_img.appendTo('.books');
+    div_data.appendTo('.books');
+    add_button.appendTo('.books');
   }
 }
+
+
+$('input#add-book').on('click', function(){
+  console.log('add-book button clicked!');
+  
+});
